c992adff0a6e11aa6c9f926d29e404a2e3851039
Priscilla Okawa <priscilla.okawa@ibm.com>
2022-03-09 13:55:02 -0800

    adds second level query for authors: AuthorType

--- server.js
+++ server.js
@@ -12,20 +12,50 @@ const app = express();
 
 //dummy data
 const books = [
-  { id: 1, name: 'Crime and Punishment', authorId: '11' },
-  { id: 2, name: 'Anna Karenina', authorId: '22' },
-  { id: 3, name: 'When Nietzsche Wept', authorId: '33' },
-  { id: 4, name: 'Animal Farm', authorId: '44' },
-  { id: 5, name: 'The Schopenhauer Cure', authorId: '33' },
+  { id: 1, name: 'Crime and Punishment', authorId: 1 },
+  { id: 2, name: 'Anna Karenina', authorId: 2 },
+  { id: 3, name: 'When Nietzsche Wept', authorId: 3 },
+  { id: 4, name: 'Animal Farm', authorId: 4 },
+  { id: 5, name: 'The Schopenhauer Cure', authorId: 3 },
+];
+
+const authors = [
+  { id: 1, name: 'Fyodor Dostoevsky' },
+  { id: 2, name: 'Leo Tolstoy' },
+  { id: 3, name: ' Irvin D. Yalom' },
+  { id: 4, name: 'George Orwell' },
 ];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
-  description: 'Book',
+  description: 'Book name',
   fields: () => ({
     id: { type: new GraphQLNonNull(GraphQLInt) },
-    name: { type: GraphQLString },
+    name: { type: new GraphQLNonNull(GraphQLString) },
     authorId: { type: new GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      //specifies a custom resolve to define how to get the author, that takes book (parent) as param:
+      resolve: (book) => {
+        return authors.find((author) => author.id === book.authorId);
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  description: 'Author name',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      //specifies a custom resolve to define how to get the author's books, that takes author (parent) as param:
+      resolve: (author) => {
+        return books.filter((book) => book.authorId === author.id);
+      },
+    },
   }),
 });
 
@@ -37,6 +67,11 @@ const RootQueryType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       description: 'Books list',
       resolve: () => books,
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: 'List of All Authors',
+      resolve: () => authors,
     },
   }),
 });
